e6f692a7653bfb590420209cc1e36b3aa4202d6a
yaninaslab <yana.belash@gmail.com>
2022-02-01 19:09:50 -0500

    Added db dump

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import dbinteractions as dbi
 username = input("Please enter your username: ")
 password = input("Please enter your password: ")
 
-if username == dbi.user[1] and password == dbi.user[2]:
+if username == dbi.users[1] and password == dbi.users[2]:
     dbi.attempt_login(username, password)
