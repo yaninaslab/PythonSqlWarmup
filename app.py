e31e68110db7d16e2e0003746f70fae4931fbab7
yaninaslab <yana.belash@gmail.com>
2022-02-01 20:59:34 -0500

    Working in class

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
+from cmath import log
 import dbinteractions as dbi
+import getpass as gp
 
 username = input("Please enter your username: ")
-password = input("Please enter your password: ")
+password = gp.getpass("Please enter your password: ")
 
-if username == dbi.users[1] and password == dbi.users[2]:
-    dbi.attempt_login(username, password)
+login_success = dbi.attempt_login(username, password)
+if(login_success == True):
+    print("Authentication success!")
+    dbi.show_items()
+    item_name = input("Please type an item name to buy: ")
+    dbi.buy_item(item_name)
+    print("Thank you, come again!")
+    exit()
+
+else:
+    print("Sorry, authentication failure")
--- dbinteractions.py
+++ dbinteractions.py
@@ -2,12 +2,49 @@ import mariadb as db
 import dbcreds
 
 
-def attempt_login(self, username, password):
+def attempt_login(username, password):
+    try:
+        conn = db.connect(user=dbcreds.user, password=dbcreds.password,
+                          host=dbcreds.host, port=dbcreds.port, database=dbcreds.database)
+        cursor = conn.cursor()
+        cursor.execute(
+            f"select id from `user` u where username = '{username}' and password = '{password}'")
+        user = cursor.fetchone()
+
+        cursor.close()
+        conn.close()
+    except:
+        print("Something went wrong!")
+
+    if(user == None):
+        return False
+    else:
+        return True
+
+
+def show_items():
     conn = db.connect(user=dbcreds.user, password=dbcreds.password,
                       host=dbcreds.host, port=dbcreds.port, database=dbcreds.database)
     cursor = conn.cursor()
-    cursor.execute(f"select username, password from `user` u")
-    users = cursor.fetchall()
+    cursor.execute(f"select id, name, description, quantity from item")
+    print("Here are the available items:")
+    items = cursor.fetchall()
+    for item in items:
+        print("")
+        print(f"{item[0]}: {item[1]} --- {item[3]}")
+        print(item[2])
+        print("")
+    cursor.close()
+    conn.close()
+
+
+def buy_item(name):
+    conn = db.connect(user=dbcreds.user, password=dbcreds.password,
+                      host=dbcreds.host, port=dbcreds.port, database=dbcreds.database)
+    cursor = conn.cursor()
+    cursor.execute(
+        f"update item set quantity = quantity - 1 where name = '{name}'")
+    conn.commit()
 
     cursor.close()
     conn.close()
